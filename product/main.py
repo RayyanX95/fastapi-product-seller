ac8f92a59f61126d398ee6ba3041802db44ec2cc
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2025-08-19 09:08:43 +0300

    Refactor database connection and enhance product management endpoints

--- product/main.py
+++ product/main.py
@@ -1,13 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
 
 from . import models, schemas
-from .database import engine
+from .database import SessionLocal, engine
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 
-@app.post("/product")
-def add(request: schemas.Product):
-    return {"message": "Product added successfully", "product": request}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/products")
+def products(db: Session = Depends(get_db)):
+    return db.query(models.Product).all()
+
+
+@app.get("/products/{id}")
+def get_product(id: int, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id).first()
+    if product:
+        return product
+    raise HTTPException(status_code=404, detail="Product not found")
+
+
+@app.post("/add_product")
+def add(request: schemas.Product, db: Session = Depends(get_db)):
+    new_product = models.Product(
+        name=request.name, description=request.description, price=request.price
+    )
+
+    print(
+        f"Adding product: {new_product.name}, Price: {new_product.price}, Description: {new_product.description}"
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return {"message": "Product added successfully", "product": new_product}
